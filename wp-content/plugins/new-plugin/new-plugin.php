54a9f28433c769ba683994434feff18210821b6f
wad2eq <mirekkedzierski@gmail.com>
2024-08-13 19:25:15 +0200

    Add new fields to plugin

--- wp-content/plugins/new-plugin/new-plugin.php
+++ wp-content/plugins/new-plugin/new-plugin.php
@@ -12,15 +12,34 @@
 class WordCounterPlugin {
 
     // ta metoda odpala się w momęcie tworzenie instancji
-    function __construct(){
+    function __construct() {
         add_action('admin_menu', array($this, 'adminPage'));
         add_action('admin_init', array($this, 'settings'));
-    }
-
+      }
 
     function adminPage(){
         add_options_page("Word Count Plugin", "Word count", "manage_options", "word-count-settings-page", array($this, 'ourHtml'));
+    }   
+    function settings(){
+        add_settings_section('wpc_first_section', null , null , 'word-count-settings-page');
+        // Word count location
+        add_settings_field( 'word_count', 'Display location', array($this, 'locationHtml'), 'word-count-settings-page', 'wpc_first_section' );
+        register_setting('wordcountplugin', 'word_count', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+        // head line text
+
+        add_settings_field( 'word_count_headline', 'Heading section', array($this, 'headLineHtml'), 'word-count-settings-page', 'wpc_first_section' );
+        register_setting('wordcountplugin', 'word_count_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics'));
+
+        add_settings_field('word_count_info', 'Info sectin', array($this, 'infoHtml'), 'word-count-settings-page', 'wpc_first_section');
+        register_setting('wordcountplugin', 'word_count_info', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Some information'));
+
     }
+    function locationHtml() { ?>
+        <select name="word_count">
+          <option value="0" <?php selected(get_option('word_count'), 0)  ?>>Beginning of post</option>
+          <option value="1" <?php selected(get_option('word_count'), 1)  ?>>End of post</option>
+        </select>
+      <?php }
     
     /**
      * To jest nasza funckcja co wyświetla html w admin panelu. 
@@ -29,7 +48,7 @@ class WordCounterPlugin {
     function ourHtml(){
         ?>
             To jest nasz plugin
-            <form action="options.php">
+            <form action="options.php" method="POST">
                 <?php
                     settings_fields('wordcountplugin'); // Jak nie damy tej lini to kliknięcie submt_button() wywali nam kupe błędów
                     do_settings_sections('word-count-settings-page');
@@ -39,33 +58,18 @@ class WordCounterPlugin {
         <?php
     }
 
-    function settings(){
-        add_settings_section('wpc_first_section', null , null , 'word-count-settings-page');
-        // Dowiedzieć się co to jest 
-        add_settings_field(
-            'word_count', 
-            'Display location', 
-            array($this, 'locationHtml'), 
-            'word-count-settings-page', 
-            'wpc_first_section' );
-        register_setting('wordcountplugin', 'word_count', array(
-            'sanitize_callback' => 'sanitize_text_field',
-            'default' => '0'
-        ));
-    }
-
-    function locationHtml(){
+    function headLineHtml() {
         ?>
-           <select name="word_count" id="word_count">
-            <option value="0">Beginign of post </option>
-            <option value="1">End of post</option>
-           </select>
+           <input type="text" name="word_count_headline" value="<?php echo esc_attr(get_option('word_count_headline')) ?>">
         <?php
-
     }
 
-
-
+    function infoHtml(){
+        ?>
+            <h2>To jest informacja o poście</h2>
+            <input type="text" value="<?php echo esc_attr(get_option('word_count_info'))   ?>" name="word_count_info"/>
+        <?php
+    }
 }
 
 new WordCounterPlugin();
