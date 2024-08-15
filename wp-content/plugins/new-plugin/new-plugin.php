6fb248ed73f17a9c25271767a22cd2cb4456fbb2
Mirek Kędzierski <miroslaw.kedzierski@acoustic.co>
2024-08-15 06:59:25 +0200

    Add common method for checkbox and custom function for sanitization

--- wp-content/plugins/new-plugin/new-plugin.php
+++ wp-content/plugins/new-plugin/new-plugin.php
@@ -24,31 +24,42 @@ class WordCounterPlugin {
         add_settings_section('wpc_first_section', null , null , 'word-count-settings-page');
         // Word count location
         add_settings_field( 'word_count', 'Display location', array($this, 'locationHtml'), 'word-count-settings-page', 'wpc_first_section' );
-        register_setting('wordcountplugin', 'word_count', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+        register_setting('wordcountplugin', 'word_count', array('sanitize_callback' => array($this, 'sanitizenLocation'), 'default' => '0'));
         // head line text
 
         add_settings_field( 'word_count_headline', 'Heading section', array($this, 'headLineHtml'), 'word-count-settings-page', 'wpc_first_section' );
         register_setting('wordcountplugin', 'word_count_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics'));
 
         //To nasze ile jest slow w naszym blog poscie
-        add_settings_field('word_count_info', 'Info section', array($this, 'infoHtml'), 'word-count-settings-page', 'wpc_first_section');
+        add_settings_field('word_count_info', 'Info section', array($this, 'checkBOX'), 'word-count-settings-page', 'wpc_first_section', array('theName' => 'word_count_info'));
         register_setting('wordcountplugin', 'word_count_info', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
 
         //Charakter couinter
-        add_settings_field('character_count_info', 'Character count info', array($this, 'characterCoutInfo'), 'word-count-settings-page', 'wpc_first_section' );
+        add_settings_field('character_count_info', 'Character count info', array($this, 'checkBOX'), 'word-count-settings-page', 'wpc_first_section', array('theName' => 'character_count_info'));
         register_setting('wordcountplugin', 'character_count_info', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
 
         //Read time
-        add_settings_field('read_time', 'Read time info', array($this, 'readTimeHtml'), 'word-count-settings-page', 'wpc_first_section' );
+        add_settings_field('read_time', 'Read time info', array($this, 'checkBOX'), 'word-count-settings-page', 'wpc_first_section', array('theName' => 'read_time' ) );
         register_setting('wordcountplugin', 'read_time', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
     }
+
+
+    // Sprawdzanie naszych danych
+    function sanitizenLocation($input){
+        if($input != 0 AND $input != 1){
+            add_settings_error('word_count', 'word_count_error', 'Display location must be the beginning or end');
+            return get_option('word_count');
+        };
+        return $input;
+    }
+
     function locationHtml() { ?>
         <select name="word_count">
           <option value="0" <?php selected(get_option('word_count'), 0)  ?>>Beginning of post</option>
           <option value="1" <?php selected(get_option('word_count'), 1)  ?>>End of post</option>
         </select>
       <?php }
-    
+
     /**
      * To jest nasza funckcja co wyświetla html w admin panelu. 
      * Narazie z admin panelu
@@ -72,19 +83,9 @@ class WordCounterPlugin {
         <?php
     }
 
-    function infoHtml(){
+    function checkBOX($args){
         ?>
-            <input type="checkbox" value="1" name="word_count_info" <?php checked(get_option('word_count_info'), '1') ?> />
-        <?php
-    }
-    function characterCoutInfo(){
-        ?>
-            <input type="checkbox" value="1" name="character_count_info" <?php checked(get_option('character_count_info'), '1') ?> />
-        <?php
-    }
-    function readTimeHtml(){
-        ?>
-            <input type="checkbox" value="1" name="read_time" id="read_time" <?php checked(get_option('read_time'), '1') ?> />
+            <input type="checkbox" value="1" name="<?php echo $args['theName'] ?>"  <?php checked(get_option($args['theName']), '1') ?> />
         <?php
     }
 }
