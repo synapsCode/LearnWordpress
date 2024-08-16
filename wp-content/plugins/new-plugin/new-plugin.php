1abb0f23a8d83a47d4bfc77a4013aea15c887488
Mirek Kędzierski <miroslaw.kedzierski@acoustic.co>
2024-08-16 07:10:55 +0200

    Wyswietlanie informacji o blogu

--- wp-content/plugins/new-plugin/new-plugin.php
+++ wp-content/plugins/new-plugin/new-plugin.php
@@ -18,26 +18,57 @@ class WordCounterPlugin {
         add_filter('the_content', array($this, 'ifWrap'));
     }
 
-
+    // TODO: $content jako właściwość obiektu;
 
     function ifWrap($content){
         if((is_main_query() AND is_single()) AND (get_option('word_count_info', '1') OR get_option('character_count_info', '1') OR get_option('read_time', '1'))){
             return $this->createHTML($content);
         }
+        return $content;
+    }
 
+    function word_counting($content){
+        $statistics_info = '';
+        $count_word = '';
+        if(get_option('word_count_info', '1') OR get_option('read_time' , '1')){
+            $count_word = str_word_count(strip_tags($content));
+        }
+
+        if(get_option('word_count_info', '1')){
+            $statistics_info = '<strong> Liczba słów: ' . $count_word . '</strong>';
+        }
+
+        if(get_option('read_time' , '1')){
+            $statistics_info = '<strong> przybliżony czas czytania: ' .'20 min' . '</strong>';
+        }
+
+        return $statistics_info;
+    }
+
+    function char_counting($content){
+        if(get_option('character_count_info', '1')){
+            $count_char = strlen(strip_tags($content));
+            return '</h3><p>Characters:'  . $count_char;
+        }
+        return '';
     }
 
     function createHTML($content){
             //Policz słowa w naszej apce
         $starTime = microtime(true);
-        $count_word = str_word_count(strip_tags($content));
+        $count_word  = '';
+
+
+        $html = '<h3>'. get_option('word_count_headline', 'Post statystics: ') . '</h3><p>' . $this->word_counting($content) . $this->char_counting($content) . '</p>';
+
         $endTime = microtime(true);
-        $html = '<h3>'. get_option('word_count_headline', 'Post statystics: ') . '</h3><p>' . $count_word . ($starTime - $endTime);
+
+        echo ( $endTime - $starTime );
                 // W zależności od ustawień modyfikujemy ustawinei od początku lub końca
         if(get_option('word_count', '0') == '0'){
             return $html . $content . 'to jest mirek';
         }
-        return $content .  $html;
+        return $content . $html;
     }
 
     function adminPage(){
@@ -48,14 +79,14 @@ class WordCounterPlugin {
         add_settings_section('wpc_first_section', null , null , 'word-count-settings-page');
         // Word count location
         add_settings_field( 'word_count', 'Display location', array($this, 'locationHtml'), 'word-count-settings-page', 'wpc_first_section' );
-        register_setting('wordcountplugin', 'word_count', array('sanitize_callback' => array($this, 'sanitizenLocation'), 'default' => '0'));
+        register_setting('wordcountplugin', 'word_count', array('sanitize_callback' => array($this, 'sanitizenLocation'), 'default' => '1'));
         // head line text
 
         add_settings_field( 'word_count_headline', 'Heading section', array($this, 'headLineHtml'), 'word-count-settings-page', 'wpc_first_section' );
         register_setting('wordcountplugin', 'word_count_headline', array('sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics'));
 
         //To nasze ile jest slow w naszym blog poscie
-        add_settings_field('word_count_info', 'Info section', array($this, 'checkBOX'), 'word-count-settings-page', 'wpc_first_section', array('theName' => 'word_count_info'));
+        add_settings_field('word_count_info', 'Word count info', array($this, 'checkBOX'), 'word-count-settings-page', 'wpc_first_section', array('theName' => 'word_count_info'));
         register_setting('wordcountplugin', 'word_count_info', array('sanitize_callback' => 'sanitize_text_field', 'default' => '1'));
 
         //Charakter couinter
